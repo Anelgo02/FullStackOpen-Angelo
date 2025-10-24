77ea722a1ec498436663ea38f1a0e34a493563e6
Angelo <1234retiu@gmail.com>
2025-10-24 18:02:59 +0200

    exercise 1.7 step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,17 +8,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [total,setTotal] = useState(0)
+
+
+  const handleAll = () =>{
+    const updatedAll = all + 1
+    setAll(updatedAll)
+  }
+
+  const handleTotal = (props) => {
+    const updateTotal = total + props
+    setTotal(updateTotal) 
+  }
 
   const handleGood = () => {
     const updateGood = good + 1
+    handleAll()
+    handleTotal(1)
     setGood(updateGood)
   }
   const handleNeutral = () => {
     const updateNeutral = neutral + 1
+    handleAll()
+    handleTotal(0)
     setNeutral(updateNeutral)
   }
   const handleBad = () => {
     const updateBad = bad + 1
+    handleAll()
+    handleTotal(-1)
     setBad(updateBad)
   }
 
@@ -33,9 +52,12 @@ const App = () => {
       </div>
 
       <h1>stats</h1>
-      <p>{good}</p>
-      <p>{neutral}</p>
-      <p>{bad}</p>
+      <p>Good feedbacks: {good}</p>
+      <p>Neutral feedbacks: {neutral}</p>
+      <p>Bad feedbacks: {bad}</p>
+      <p>All feedbacks: {all}</p>
+      <p>Average: {total/all} </p>
+      <p>Positive: {(good/all) * 100}%</p>
 
 
     </div>
